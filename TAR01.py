d224f9becc14b6efd1717179fd017d54635ce455
KUKO1000 <112733337+KUKO1000@users.noreply.github.com>
2022-09-19 23:18:35 -0400

    Add files via upload

---
+++ TAR01.py
@@ -0,0 +1,18 @@
+"""Ismael navarro
+   Matricula:21-1689 
+   
+"""
+
+
+n1 = int(input("Primer numero =  "))
+n2 = int(input("Segundo numero =  "))
+Suma = n1+n2
+Resta= n1-n2
+Mult=n1*n2
+Division=n1/n2
+
+
+print("la suma %s y %s es %s" % (n1,n2,Suma))
+print("la resta %s y %s es %s" % (n1,n2,Resta))
+print("la multiplicacion %s y %s es %s" % (n1,n2,Mult))
+print("la division %s y %s es %s" % (n1,n2,Division))
