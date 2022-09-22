ab4c52837f349ba8e813f8523061b74823664ebb
KUKO1000 <112733337+KUKO1000@users.noreply.github.com>
2022-09-22 01:23:38 -0400

    Add files via upload

---
+++ TAREA 2.py
@@ -0,0 +1,19 @@
+
+# ISMAEL NAVAROO
+#Matricula: 21-1689
+
+import math
+
+n1=float(input("Escribir el primer numero  :"))
+n2=float(input("Escribir el Segundo  numero  :"))
+
+A = n1**2
+B=  n2**2
+
+print("Resultado del la raiz" , n1,"=",round(A))
+print("Resultado del la raiz" , n2,"=",round(B))
+
+C= A + B
+
+print("la suma de ",A,'+',B,"es de = ", A + B)
+print("el resultado de la raiz de ",C,"es =",math.sqrt(C))
